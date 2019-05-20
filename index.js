3c8c2b895474064b9eec0226d9370ab476b7712f
Nathan <nathan.sprenkle@gmail.com>
2019-05-20 16:21:19 -0400

    Add support for numbers less than 10

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function numberToString() { }
+function numberToString(number) {
+  switch (number) {
+    case 0:
+      return 'zero'
+    case 1:
+      return 'one'
+    case 2:
+      return 'two'
+    case 3:
+      return 'three'
+    case 4:
+      return 'four'
+    case 5:
+      return 'five'
+    case 6:
+      return 'six'
+    case 7:
+      return 'seven'
+    case 8:
+      return 'eight'
+    case 9:
+      return 'nine'
+  }
+}
 
 module.exports = { numberToString } 
--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,16 @@
 const uut = require('./index')
 
 describe('numberToString', () => {
-  it('does not break', () => {
-    uut.numberToString()
+  it('can handle numbers less than 10', () => {
+    expect(uut.numberToString(0)).toEqual('zero')
+    expect(uut.numberToString(1)).toEqual('one')
+    expect(uut.numberToString(2)).toEqual('two')
+    expect(uut.numberToString(3)).toEqual('three')
+    expect(uut.numberToString(4)).toEqual('four')
+    expect(uut.numberToString(5)).toEqual('five')
+    expect(uut.numberToString(6)).toEqual('six')
+    expect(uut.numberToString(7)).toEqual('seven')
+    expect(uut.numberToString(8)).toEqual('eight')
+    expect(uut.numberToString(9)).toEqual('nine')
   })
 })
