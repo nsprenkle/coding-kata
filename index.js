efdd54e4ef67b0390ed16344da9a065204806c86
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-05-24 11:00:27 -0400

    Refactor magnitude handling logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ONES = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 const TENS = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 const TEENS = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+const MAGNITUEDS = ['', 'thousand']
 
 function numberToString(number) {
   let stringElements = []
@@ -10,6 +11,14 @@ function numberToString(number) {
     return 'zero'
   }
 
+  stringElements.push(...handleMagnitude(number, MAGNITUEDS[0]))
+
+  // post-process
+  return stringElements.join(' ').trim()
+}
+
+function handleMagnitude(number) {
+  let stringElements = []
   // handle teens independently
   if (number % 100 > 10 && number % 100 < 20) {
     stringElements.unshift(TEENS[number % 10])
@@ -26,13 +35,12 @@ function numberToString(number) {
   // handle hundreds
   number = Math.floor(number / 10)
 
-  if(number > 0) {
+  if (number > 0) {
     stringElements.unshift('hundred')
     stringElements.unshift(ONES[number % 10])
   }
 
-  // post-process
-  return stringElements.join(' ').trim()
+  return stringElements
 }
 
 module.exports = { numberToString } 
