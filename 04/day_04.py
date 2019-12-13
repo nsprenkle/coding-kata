863d9257b29a26bbb8d1cb4cc9244c06cb834c77
Nathan Sprenkle <nathan.sprenkle@gmail.com>
2019-12-13 13:52:59 -0500

    Add day 04 part 2

--- 04/day_04.py
+++ 04/day_04.py
@@ -3,23 +3,36 @@ def matches_password_criteria(password):
     password = str(password)
 
     # two adjacent digits are the same
-    adjacent_digits = False
+    adjacent_pair = False
     numbers_increasing = True
 
     prev_digit = None
+    number_count = 0
 
     for digit in password:
-        # password must contain an adjacent repeated digit
+        # password must contain an adjacent pair of digits
         if digit == prev_digit:
-            adjacent_digits = True
+            if number_count == 0:
+                number_count = 2
+            else:
+                number_count += 1
+        else:
+            if number_count == 2:
+                adjacent_pair = True
+            number_count = 0
 
         # numbers must only increase from left to right
         if digit < prev_digit:
+            print('Password not strictly increasing, {}'.format(password))
             return False
 
         prev_digit = digit
 
-    return adjacent_digits & numbers_increasing
+    print('Password {} a pair {}'.format(
+        'contains' if adjacent_pair else 'does not contain', password))
+    adjacent_pair = adjacent_pair or number_count == 2
+
+    return adjacent_pair and numbers_increasing
 
 
 def main(min, max):
--- 04/day_04_test.py
+++ 04/day_04_test.py
@@ -2,7 +2,8 @@ import day_04
 
 
 def test_matches_password_criteria():
-    assert day_04.matches_password_criteria(111111)
+    assert day_04.matches_password_criteria(112233)
+    assert not day_04.matches_password_criteria(123444)
+    assert day_04.matches_password_criteria(111122)
     assert not day_04.matches_password_criteria(223450)
     assert not day_04.matches_password_criteria(123456)
-    assert not day_04.matches_password_criteria(123789)
